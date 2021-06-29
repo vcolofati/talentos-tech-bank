2e098245fb0d4553ef7922cce2ac4529edb25c92
Victor <vcolofati@gmail.com>
2021-06-28 22:05:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# talentos-tech-bank
---
+++ src/day0/Main.java
@@ -0,0 +1,9 @@
+package day0;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(System.getProperty("java.version"));
+    }
+}
